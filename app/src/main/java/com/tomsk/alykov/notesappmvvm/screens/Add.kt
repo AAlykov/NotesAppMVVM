8849597a59c1bbc720fadb0d67af3bfef3301433
Alykov <aalykov@gmail.com>
2022-09-06 08:10:29 +0300

    Lesson 1

---
+++ app/src/main/java/com/tomsk/alykov/notesappmvvm/screens/Add.kt
@@ -0,0 +1,9 @@
+package com.tomsk.alykov.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Add(navController: NavHostController) {
+}
---
+++ app/src/main/java/com/tomsk/alykov/notesappmvvm/screens/Main.kt
@@ -0,0 +1,10 @@
+package com.tomsk.alykov.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Main(navController: NavHostController) {
+
+}
